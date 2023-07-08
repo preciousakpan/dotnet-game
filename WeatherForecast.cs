5d71b2e243fd6ee60e5483a7ba36ec0fd7fd0b2d
preciousakpan <precyudy@gmail.com>
2023-07-08 15:27:14 +0100

    trunk added

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,7 +2,13 @@ namespace dotnet_game;
 
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+        public WeatherForecast(DateOnly date, int temperatureC) 
+        {
+            this.Date = date;
+                this.TemperatureC = temperatureC;
+               
+        }
+            public DateOnly Date { get; set; }
 
     public int TemperatureC { get; set; }
 
